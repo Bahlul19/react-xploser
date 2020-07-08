9149d60cdaeceaef1f95197f8c1f29a5fa70e59a
Bahlul19 <bahlul.tausif@gmail.com>
2020-07-08 18:09:42 +0600

    Implemented the functional component code

---
+++ src/components/Calculation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Calculation()
+{
+    var sum = (a+b)/c;
+    return sum;
+}
+
+var a = 5;
+var b = 10;
+var c = 2;
+
+export default Calculation; 
---
+++ src/components/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Hello()
+{
+    return <h1>This component is create a by Tausif </h1>
+}
+
+export default Hello;
